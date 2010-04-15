cb260649426ba9e98d82ff94c8a22e2da43d738e
Georg Leciejewski <gl@salesking.eu>
2010-04-15 14:35:50 +0200

    fixed integer casting in view hook, added de, removed unused lang/folder

--- lib/issues_vote_hook.rb
+++ lib/issues_vote_hook.rb
@@ -2,8 +2,8 @@
 class IssuesVoteHook < Redmine::Hook::ViewListener
   render_on :view_issues_show_details_bottom, :inline => <<-END
     <tr><td><b><%= l :label_votes %>:</b></td><td>
-    <% vv = @issue.votes_value %>
-    <%= content_tag('span', vv, :class => (vv > 0? 'votes-positive': ( vv < 0 ? 'votes-negative' : ''))) %>
+    <% vv = @issue.votes_value.to_i %>
+    <%= content_tag('span', vv, :class => (vv > 0 ? 'votes-positive': ( vv < 0 ? 'votes-negative' : ''))) %>
     <% @project = @issue.project %>
     <% if authorize_for('issues', 'vote') && !@issue.voted_by_user? %>
     <%= link_to("", {:controller => 'issues', :action => 'vote', :id => @issue, :vote => :up}, :class => 'icon icon-vote-up') %>
